e3a3a692b50424176be4c649da9d50af85bfcd62
Ayofe Faheez <ayofefaheez@gmail.com>
2024-03-15 20:08:28 -0800

    functions updated

--- script.js
+++ script.js
@@ -1,79 +1,165 @@
-let currentDisplay = "";
+let currentDisplay = "0";
 let newValue = document.querySelector(".resultContent");
 let calculation = document.querySelector(".calculation");
-newValue.value = "0";
-let val1, val2;
-let valuesToClear = [];
+let val1, val2, operands;
+let values = [];
+let resultDisplay = false;
+calculation.innerHTML = '';
+displayUpdate();
 
+//Disable cursor movement
 newValue.onselect = function (event) {
   event.preventDefault();
 };
 
+//Display current numbers
 function appendValue(value) {
-  valuesToClear.push(value)
-  if (currentDisplay === "") {
-    currentDisplay = value;
-  } else {
-    if (currentDisplay.slice(0, 1) > 0) {
-      currentDisplay = valuesToClear.join('');
-    } else {
-      currentDisplay = value;
-    }
+  // Check if calculation results are displayed and if their index equals a certain value.
+  if (resultDisplay && calculation.innerHTML.slice(-1) === '=') {
+    calculation.innerHTML = '';
   }
-
+  //each number clicked enter the value array
+  values.push(value)
+  // If the first character of the current display is '0', update values and currentDisplay with the new value
+  if (currentDisplay.slice(0, 1) === '0') {
+    values = [value];
+    currentDisplay = value;} 
+  // otherwise, update currentDisplay
+  else {
+    currentDisplay = values.join(''); }
   displayUpdate();
-  console.log(valuesToClear)
 }
 
+//Update's Input content
 function displayUpdate() {
   newValue.value = currentDisplay;
 }
 
+//Display the value1 with the operator clicked
 function displayCal(cal) {
-  currentDisplay = "";
-  valuesToClear = [];
-  calculation.innerHTML = newValue.value + cal;
-  val1 = newValue.value;
-  console.log(val1);
-  console.log(calculation.innerHTML);
+  if (calculation.innerHTML === '') {
+    calculation.innerHTML = newValue.value + cal;
+  } else {
+    calculation.innerHTML += cal
+  }
+    val1 = newValue.value;
+    currentDisplay = "0";
+    values = [];
 }
 
+//Updates calculation result
 const calculateResult = function () {
-  val2 = valuesToClear.join('');
-  console.log(val2);
-  let operands = calculation.innerHTML.slice(-2);
-  console.log(operands);
-  calculation.innerHTML += val2 + " " + "=";
-  math(val1, val2, operands);
+  if (calculation.innerHTML.slice(-1) === ' ') {
+    val2 = newValue.value;
+    operands = calculation.innerHTML.split(' ').at(1);
+    console.log(operands);
+    calculation.innerHTML += val2 + " " + "=";
+    math(val1, val2, operands);
+    console.log(val1,val2, calculation.innerHTML)
+  } else {
+    val1 = newValue.value
+    val2 = calculation.innerHTML.split(' ').at(-2);
+    operands = calculation.innerHTML.split(' ').at(1);
+    let upadatedCalculation = calculation.innerHTML.split(' ')
+    upadatedCalculation[0] = val1;
+    calculation.innerHTML = upadatedCalculation.join(' ')
+    math(val1, val2, operands);
+  }
 };
 
+//Perform the calculation
 const math = function (a, b, cal) {
-  currentDisplay = `${Number(a) + cal + Number(b)}`;
-  if (currentDisplay.includes("+")) {
-    currentDisplay = Number(a) + Number(b);
-  } else if (currentDisplay.includes("-")) {
-    currentDisplay = Number(a) - Number(b);
-  } else if (currentDisplay.includes("/")) {
-    currentDisplay = Number(a) / Number(b);
-  } else if (currentDisplay.includes("*")) {
-    currentDisplay = Number(a) * Number(b);
-  }
+  if (cal === '+') {
+    currentDisplay = `${Number(a) + Number(b)}`;
+  } else if (cal === '-') {
+      currentDisplay = `${Number(a) - Number(b)}`;
+    } else if (cal === '/') {
+      currentDisplay = `${Number(a) / Number(b)}`;
+    } else if (cal === '*') {
+      currentDisplay = `${Number(a) * Number(b)}`;
+    }
+  values = []
   displayUpdate();
-  console.log(currentDisplay); 
+  resultDisplay = true;
+  console.log(currentDisplay)
 }
 
 document.querySelector('.backspace').addEventListener('click', function() {
   if (calculation.innerHTML.slice(-1) ==='=') {
     calculation.innerHTML = '';
   } else {
-    if (valuesToClear.length  === 1) {
+    if (values.length === 1) {
       newValue.value = '0'
-      valuesToClear = []
+      values = []
     } else {
-      valuesToClear.pop()
-      newValue.value = valuesToClear.join('');
+      values.pop()
+      newValue.value = values.join('');
     }
   }
-  console.log(calculation.innerHTML.slice(-2))
-  console.log(valuesToClear)
 })
+
+//Clear the most recent entry
+function clearEntry() {
+  if (calculation.innerHTML.slice(-1) === '=') {
+    currentDisplay = "";
+    values = [];
+    calculation.innerHTML = '';
+    newValue.value = "0";
+  } else{
+    newValue.value = "0";
+    values = [];
+  }
+}
+
+//Clear all the input
+function Clear() {
+  currentDisplay = "";
+  values = [];
+  calculation.innerHTML = '';
+  newValue.value = "0";
+}
+
+function bigMath (func) {
+  if(func === '%') {
+    newValue.value =  newValue.value * (1/100)
+  } else if (func === 'reciprocal') {
+    calculation.innerHTML = `1/(${newValue.value})`
+    newValue.value = 1/newValue.value
+  }
+  else if (func === 'square') { 
+    calculation.innerHTML = `sqr(${newValue.value})`
+    newValue.value = newValue.value * newValue.value;
+  }
+  else if (func === 'squareRoot') {
+    calculation.innerHTML = `√(${newValue.value})`
+    newValue.value = Math.sqrt(newValue.value)
+  }
+  values = []
+}
+
+const changeSignBtn = document.getElementById("changeSignBtn");
+const changeSignInput = document.getElementById("changeSignInput");
+
+changeSignBtn.addEventListener("click", function() {
+  if (changeSignInput.checked) {
+    if (calculation.innerHTML.slice(-1) === '=') {
+      calculation.innerHTML = calculation.innerHTML.includes('negate') ? `negate(${calculation.innerHTML})` : `negate(${newValue.value})` 
+      newValue.value = newValue.value.replace('-', '');
+    } else {
+      newValue.value = newValue.value.replace('-', '');
+    }
+    changeSignInput.checked = false; // Uncheck the checkbox
+  } else {
+    if (calculation.innerHTML.slice(-1) === '=') {
+      calculation.innerHTML = `negate(${newValue.value})`
+      newValue.value = `-${newValue.value}`;
+    }  else if (calculation.innerHTML.includes('negate')) {
+      calculation.innerHTML = `negate(${calculation.innerHTML})`
+      newValue.value = `-${newValue.value}`;
+    }
+    else {
+      newValue.value = `-${newValue.value}`;
+    }
+    changeSignInput.checked = true; // Check the checkbox
+  }
+});
